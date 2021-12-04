2f7acc2cf3b7eb7ca9f335ddc4c1052e09e7611b
Nicolás <nicolasmlambertucci@gmail.com>
2021-12-03 22:00:02 -0300

    added get and post

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,10 +1,31 @@
 const {Router} = require('express')
 const router = Router()
 
-const data = require('../sample.json')
+const movies = require('../sample.json')
 
 router.get('/',(req,res)=>{
-    res.json(data)
+    res.status(200).json({
+        "message": "Movies retrieved",
+        "model": movies
+    })
+})
+
+router.post('/',(req,res) =>{
+    const {title, director, year, rating } = req.body
+    if(title && director && year && rating){
+        const id = movies.length + 1
+        const newMovie = {id, ...req.body}
+        movies.push(newMovie)
+        res.status(200).json({
+            "message": "Movie saved",
+            "model": movies
+        })
+    }else{
+        res.status(500).json({
+            "message": "Internal server error"
+        })
+    }
+    res.send("success")
 })
 
 module.exports = router
