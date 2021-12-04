0c5fbc6dd8efe18e1edeb53c64413cb5a79c3ca1
Nicolás <nicolasmlambertucci@gmail.com>
2021-12-03 22:20:18 -0300

    finished API

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const router = Router()
+const _ = require('underscore')
+
 
 const movies = require('../sample.json')
 
@@ -27,5 +29,47 @@ router.post('/',(req,res) =>{
     }
     res.send("success")
 })
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    if (id && title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id === id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.status(200).json({
+            "message": "Movie updated",
+            "model": movies
+        })
+    } else {
+        res.status(500).json({
+            "message": "Internal server error"
+        })
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    if (id) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movies.splice(i, 1);
+            }
+        });
+        res.status(200).json({
+            "message": "Movie deleted",
+            "model": movies
+        })
+    }else{
+        res.status(500).json({
+            "message": "Internal server error"
+        })
+    }
+});
 
 module.exports = router
